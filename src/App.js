72779e504b6076f905b12add8e9cbb7f03639e9e
Farhad <farhadg@the-ifc.com>
2024-04-07 16:15:51 +0200

    with material components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 import './App.css';
+import { TextField, Alert, Button, Card, CardContent } from '@mui/material';
 
 function App() {
   return (
     <div className="app">
       <div className="container">
         <div className="title">
-          titre
+          <h1>Devine le nombre</h1>
         </div>
         <div className="main">
             <div className="input">
-              input
+              <TextField id="outlined-basic" label="Outlined" variant="outlined" style={{width: '100%'}} />
             </div>
             <div className="result">
-              resultat
+              <Alert severity="info" variant="outlined" style={{width: '100%'}}>This is an info Alert.</Alert>
             </div>
             <div className="buttons">
               <div className="button">
-                check
+                <Button variant="contained">Check</Button>
               </div>
               <div className="button">
-                restart
+                <Button variant="contained">Restart</Button>
               </div>
             </div>
             <div className="history">
-              <div className="historyItem">
+              <Card className="historyItem">
                 12
-              </div>
-              <div className="historyItem">
+              </Card>
+              <Card className="historyItem">
                 13
-              </div>
-              <div className="historyItem">
+              </Card>
+              <Card className="historyItem">
                 54
-              </div>
-              <div className="historyItem">
-              </div>
-              <div className="historyItem">
-              </div>
-              <div className="historyItem">
-              </div>
-
+              </Card>
             </div>
         </div>  
       </div>
