21dedd6e2991774edabbaacb843689f662843d2c
Newsuprano <newsuprano.yt@gmail.com>
2024-05-05 11:53:52 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,44 +1,111 @@
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import CheckIcon from '@mui/icons-material/Check';
+import ReplayIcon from '@mui/icons-material/Replay';
+import Alert from '@mui/material/Alert';
+import Card from '@mui/material/Card';
+import React, {useState} from 'react'
+
 
 function App() {
-  return (
+
+const [randomNumber, setRandomNumber] = useState(generateNumber(100));
+const [input, setInput] = useState("");
+const [history, setHistory] = useState([]);
+const [result, setResult] = useState("");
+
+  function generateNumber(max) {
+    return Math.floor(Math.random() * max);
+}
+console.log(randomNumber);
+console.log(history);
+console.log(result);
+
+function onValidate(input, randomNumber, newHistory) {
+  if (input > randomNumber) {
+    setResult("Trop grand")
+  }
+  if (input < randomNumber) {
+    setResult("Trop petit")
+  }
+  if (input == randomNumber) {
+    setResult(`Bravo, tu as trouve le nombre en ${newHistory.length} essais.`)
+  }
+}  
+
+
+return (
     <div className="app">
       <div className="container">
-        <div className="title">
-          titre
+      <div className="title">
+          <h1>GUESS THE NUMBER</h1>
         </div>
         <div className="main">
             <div className="input">
-              input
+            <TextField
+            style={{width:"100%"}}
+          id="outlined-number"
+          label="Number"
+          type="number"
+          min="0"
+          max="100"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value)
+          }}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
             </div>
+            {result.length > 0 && (
             <div className="result">
-              resultat
+            <Alert 
+            severity="info" 
+            style={{width:"100%"}}>
+            {result}.
+            </Alert>
             </div>
+            )}
             <div className="buttons">
               <div className="button">
-                check
-              </div>
-              <div className="button">
-                restart
-              </div>
-            </div>
-            <div className="history">
-              <div className="historyItem">
-                12
-              </div>
-              <div className="historyItem">
-                13
-              </div>
-              <div className="historyItem">
-                54
-              </div>
-              <div className="historyItem">
-              </div>
-              <div className="historyItem">
-              </div>
-              <div className="historyItem">
+              <Button 
+              aria-label="contained" 
+              variant="outlined" 
+              startIcon={<CheckIcon />}
+              id="checkButton"
+              onClick={() => {
+                const newHistory = [...history];
+              newHistory.push(input);
+              setHistory(newHistory);
+              onValidate(input, randomNumber, newHistory);
+              }}>
+              Check
+              </Button>
               </div>
 
+              <div className="button">
+              <Button 
+              aria-label="contained" 
+              variant="outlined" 
+              startIcon={<ReplayIcon />}
+              onClick={() => {
+                let newRandomNumber = generateNumber(100)
+                console.log(newRandomNumber);
+                setRandomNumber(newRandomNumber);
+                setHistory([]);
+                setInput("");
+                setResult("");
+              }}>
+                Restart
+              </Button>
+              </div>
+              </div>
+              <div className="history">
+              {history.map((value, index) => (
+                <Card className="historyItem" key={index}> {value} </Card>
+              ))}
             </div>
         </div>  
       </div>
